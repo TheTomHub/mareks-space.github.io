25f2d4cc2eb0bafb0bb7d17a6e07d2c0c823fe4f
Marek Dlugos <marek.dlugos@gmail.com>
2016-11-03 10:32:57 +0900

    Added Smartlook analyok analytics

--- js/analytics.js
+++ js/analytics.js
@@ -5,3 +5,11 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-85704034-1', 'auto');
 ga('send', 'pageview');
+
+
+window.smartlook||(function(d) {
+var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+c.charset='utf-8';c.src='//rec.smartlook.com/recorder.js';h.appendChild(c);
+})(document);
+smartlook('init', '19a7b52b8719694d43528f836c07f514dd1a6990');
